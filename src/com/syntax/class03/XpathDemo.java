1dc63b704e4b3d70932bc87a09744cac81e604e6
basakalemdag <basakalemdag@gmail.com>
2022-10-29 02:47:48 +0300

    second commit

--- src/com/syntax/class03/XpathDemo.java
+++ src/com/syntax/class03/XpathDemo.java
@@ -1,6 +1,27 @@
 package com.syntax.class03;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class XpathDemo {
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "Drivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        // navigate to fb.com
+        driver.get("https://www.facebook.com/");
+
+        // enter the username (via Xpath)
+        driver.findElement(By.xpath("//input[@id='email']")).sendKeys("Bianca");
+
+        // click on Forgot Password?
+        driver.findElement(By.xpath("//a[text()='Şifreni mi Unuttun?']")).click();
+
+
+
+    }
 
 
 }
