c423b03026a1fb1607cf3d9cb7f53b5cc81270d8
basakalemdag <basakalemdag@gmail.com>
2022-10-29 05:35:11 +0300

    second commit

--- src/com/syntax/class03/HwXPath.java
+++ src/com/syntax/class03/HwXPath.java
@@ -1,12 +1,45 @@
 package com.syntax.class03;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class HwXPath {
 
-    // Navigate to http://syntaxprojects.com/
-    // Using Xpath:
-    //Click on start practicing
-    //click on simple form demo
-    //enter any text on first text box
-    //click on show message
-    //quit the browser
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "Drivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        // Navigate to http://syntaxprojects.com/
+        driver.get("http://syntaxprojects.com/");
+
+        // maximize window:
+        driver.manage().window().maximize();
+
+        // Using Xpath:
+        //Click on start practicing
+        driver.findElement(By.xpath("//a[@id='btn_basic_example']")).click();
+
+        //click on simple form demo
+        // driver.findElement(By.xpath("(//a[text()='Simple Form Demo'])[2]")).click();
+
+        Thread.sleep(5000);
+
+        //click on simple form demo using AND Operator with Xpath
+        driver.findElement(By.xpath("//a[@class='list-group-item' and @href='basic-first-form-demo.php']")).click();
+
+        //enter any text on first text box
+        driver.findElement(By.xpath("//input[@id='user-message']")).sendKeys("Example Text");
+
+        //click on show message
+        driver.findElement(By.xpath("//button[text()='Show Message']")).click();
+
+        Thread.sleep(3000);
+
+        //quit the browser
+        driver.quit();
+
+
+    }
+
 }
