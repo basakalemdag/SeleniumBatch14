16c919cee02595751208057c7a0965291f40c84d
basakalemdag <basakalemdag@gmail.com>
2022-10-29 03:01:09 +0300

    second commit

---
+++ src/com/syntax/class03/HwXPath.java
@@ -0,0 +1,12 @@
+package com.syntax.class03;
+
+public class HwXPath {
+
+    // Navigate to http://syntaxprojects.com/
+    // Using Xpath:
+    //Click on start practicing
+    //click on simple form demo
+    //enter any text on first text box
+    //click on show message
+    //quit the browser
+}
